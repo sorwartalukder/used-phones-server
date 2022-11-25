cd4b0fcc38d4946e1b33c94ff872e14a4ded8d90
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-26 01:20:23 +0600

    send my product database to client

--- index.js
+++ index.js
@@ -22,23 +22,32 @@ async function run() {
     try {
         const userCollection = client.db('usedPhones').collection('users')
         const productCollection = client.db('usedPhones').collection('products')
+        //send my product client
+        app.get('/products', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        })
 
         // add product database
         app.post('/products', async (req, res) => {
             const product = req.body;
-
-
+            //call users
             const query = { email: product.email }
             const user = await userCollection.findOne(query);
+
             if (user.role === 'Seller') {
                 const result = await productCollection.insertOne(product);
                 return res.send(result)
             }
             res.status(401).send({ message: 'unauthorized access' });
         })
+
         // add user database 
         app.post('/users', async (req, res) => {
             const newUser = req.body;
+
             const query = { email: newUser.email }
             const user = await userCollection.findOne(query);
             if (!user) {
